2e5ae677da1e5f44efb03263a4e0a1649b47c053
Arun Shankar <arun@fuiszmedia.com>
2016-12-05 08:36:11 -0800

    only show the button when the number of files are greater than 1

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,16 +1,19 @@
 (function () {
   document.getElementById('fileUpload').onchange = function () {
     document.getElementById('fileSelectionStatus').innerHTML = this.files.length + ' files selected'
+    if (this.files.length > 0) {
+      $('.uploadFileButton').css({
+        display: 'block'
+      })
+    }
   };
 
   $('.performDetectButton').click((e) => {
     var imageData = JSON.stringify($('.performDetectButton').data('image'))
     $.post( "/detect", { image: imageData })
       .done(data => {
-        console.log('the return data is ', data)
         if (data && data.results) {
           var images = data.results.images
-          // var imagesView = []
           var imageHTML = ''
           images.map((image, i) => {
             imageHTML += `<div class="hamDistance">${(i + 1) + ') Similarity Measure: ' + image.hamDistance}</div>`
